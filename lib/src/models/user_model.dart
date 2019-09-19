3101c58536f48a4033306488c625388fbd667061
stephan-kashkarov <kashkarovwork@gmail.com>
2019-09-19 12:39:18 +1000

    inital commit

---
+++ lib/src/models/user_model.dart
@@ -0,0 +1,24 @@
+import 'package:intl/intl.dart';
+import 'package:password/password.dart';
+
+class UserModel {
+	int id;
+	String name;
+	String bio;
+	String email;
+	String passhash;
+	String createdAt;
+
+	UserModel(password) {
+		this.id = 0;
+		this.name = "";
+		this.bio = "";
+		this.email = "";
+		this.passhash = Password.hash(password, new PBKDF2());
+		this.createdAt = DateFormat('HH:mm:ss | dd.MM.yyyy').format(new DateTime.now())
+	}
+
+	bool checkPassword (password) {
+		return Password.verify(password, this.passhash);
+	}
+}
